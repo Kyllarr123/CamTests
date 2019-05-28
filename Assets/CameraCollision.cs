e91a745273b99911f70801dfe47c1d8d9eeaa18e
Kyllarr123 <russell.ayres.90@gmail.com>
2019-05-28 20:11:51 +1000

    CamTest

---
+++ Assets/CameraCollision.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class CameraCollision : MonoBehaviour
+{
+
+    public float minDist;
+
+    public float maxDist;
+
+    public float smoothing;
+
+    public Vector3 direction;
+
+    public float distance;
+
+    public Camera cam;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = transform.localPosition.normalized;
+        distance = transform.localPosition.magnitude;
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredPos = transform.parent.TransformPoint(direction * maxDist);
+        RaycastHit hit;
+        if (Physics.Linecast(transform.parent.position, desiredPos, out hit))
+        {
+            distance = Mathf.Clamp((hit.distance * 0.9f), minDist, maxDist);
+
+        }
+        else
+        {
+            distance = maxDist;
+        }
+
+        transform.localPosition =
+            Vector3.Lerp(transform.localPosition, direction * distance, Time.deltaTime * smoothing);
+
+        float c = cam.nearClipPlane;
+        Transform camPos = cam.transform;
+        bool clipping = true;
+
+        while (clipping)
+        {
+            Vector3 pos1 = cam.ViewportToWorldPoint(new Vector3(0, 0, c));
+            Vector3 pos2 = cam.ViewportToWorldPoint(new Vector3(0.5f, 0, c));
+            Vector3 pos3 = cam.ViewportToWorldPoint(new Vector3(1, 0, c));
+            Vector3 pos4 = cam.ViewportToWorldPoint(new Vector3(0, 0.5f, c));
+            Vector3 pos5 = cam.ViewportToWorldPoint(new Vector3(1, 0.5f, c));
+            Vector3 pos6 = cam.ViewportToWorldPoint(new Vector3(0, 1, c));
+            Vector3 pos7 = cam.ViewportToWorldPoint(new Vector3(0.5f, 1, c));
+            Vector3 pos8 = cam.ViewportToWorldPoint(new Vector3(1, 1, c));
+        
+        
+            Debug.DrawLine(camPos.position, pos1, Color.red);
+            Debug.DrawLine(camPos.position, pos2, Color.red);
+            Debug.DrawLine(camPos.position, pos3, Color.red);
+            Debug.DrawLine(camPos.position, pos4, Color.red);
+            Debug.DrawLine(camPos.position, pos5, Color.red);
+            Debug.DrawLine(camPos.position, pos6, Color.red);
+            Debug.DrawLine(camPos.position, pos7, Color.red);
+            Debug.DrawLine(camPos.position, pos8, Color.red);
+
+            if (Physics.Linecast(camPos.position, pos1, out hit))
+            {
+                
+            } else if (Physics.Linecast(camPos.position, pos2, out hit))
+            {
+                
+            }else if (Physics.Linecast(camPos.position, pos3, out hit))
+            {
+                
+            }else if (Physics.Linecast(camPos.position, pos4, out hit))
+            {
+                
+            }else if (Physics.Linecast(camPos.position, pos5, out hit))
+            {
+                
+            }else if (Physics.Linecast(camPos.position, pos6, out hit))
+            {
+                
+            }else if (Physics.Linecast(camPos.position, pos7, out hit))
+            {
+                
+            }else if (Physics.Linecast(camPos.position, pos8, out hit))
+            {
+                
+            }
+            else
+            {
+                clipping = false;
+            }
+
+            if (clipping)
+            {
+                transform.localPosition += camPos.forward * 0.3f;
+            }
+        }
+
+
+    }
+}
---
+++ Assets/CameraControl.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float cameraSpeed = 120f;
+
+    public GameObject followObj;
+
+    public Vector3 followPosition;
+
+    public float clamp = 80;
+
+    public float sensitivity = 150;
+
+    public GameObject camera;
+
+    public GameObject player;
+
+    public float disX;
+    public float disY;
+    public float disZ;
+    public float mouseX;
+    public float mouseY;
+    public float finalInputX;
+    public float finalInputZ;
+    public float smoothX;
+    public float smoothY;
+    public float rotY = 0.0f;
+    public float rotX = 0.0f;
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 rotation = transform.rotation.eulerAngles;
+        rotX = rotation.x;
+        rotY = rotation.y;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float inputX = Input.GetAxis("JoyStickHori");
+        float inputZ = Input.GetAxis("JoyStickVert");
+        
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        finalInputX = inputX + mouseX;
+        finalInputZ = inputZ + mouseY;
+
+        rotY += finalInputX * sensitivity * Time.deltaTime;
+        rotX += finalInputZ * sensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clamp, clamp);
+
+        Quaternion localRot = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRot;
+
+    }
+
+    private void FixedUpdate()
+    {
+        UpdatePos();
+    }
+
+    private void UpdatePos()
+    {
+        Transform target = followObj.transform;
+
+        float step = cameraSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
